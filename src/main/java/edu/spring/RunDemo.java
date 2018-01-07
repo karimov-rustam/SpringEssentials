af6ebb249e5e501ad8c6e0296c3c115130473ca3
Karimov R <rkarimov.r@gmail.com>
2018-01-07 13:09:10 +0400

    Add factory-method & factory-bean to applicationContext.xml

--- src/main/java/edu/spring/RunDemo.java
+++ src/main/java/edu/spring/RunDemo.java
@@ -1,23 +1,31 @@
 package edu.spring;
 
-import edu.spring.entities.Game;
+import edu.spring.config.AppConfig;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.text.NumberFormat;
 
 public class RunDemo {
     public static void main(String[] args) {
-//        AnnotationConfigApplicationContext context =
-//                new AnnotationConfigApplicationContext(AppConfig.class);
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Game game = context.getBean("game", Game.class);
-        System.out.println(game.playGame());
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        System.out.println("There are " + context.getBeanDefinitionCount() + " beans");
-        for (String name :
-                context.getBeanDefinitionNames()) {
-            System.out.println(name);
-        }
+        NumberFormat nf = context.getBean(NumberFormat.class);
+        double amount = 12345678.9012345;
+        System.out.println(nf.format(amount));
+
+//        Team royals = context.getBean("royals", Team.class);
+//
+//        Game game1 = context.getBean("game", Game.class);
+//        System.out.println(game1.playGame());
+//
+//        Game game2 = context.getBean("game", Game.class);
+//        game2.setAwayTeam(royals);
+//        System.out.println(game2.playGame());
+//
+//        System.out.println(game1);
     }
 }
--- src/main/java/edu/spring/config/AppConfig.java
+++ src/main/java/edu/spring/config/AppConfig.java
@@ -1,25 +1,42 @@
 package edu.spring.config;
 
+import edu.spring.entities.BaseballGame;
+import edu.spring.entities.Game;
+import edu.spring.entities.RedSox;
+import edu.spring.entities.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.text.NumberFormat;
+import java.util.List;
 
 @Configuration
 @ComponentScan(basePackages = "edu.spring")
 public class AppConfig {
 
-//    @Autowired
-//    private DataSource dataSource;
-//
-//    @Autowired @Qualifier("redSox")
-//    private Team home;
-//
-//    @Autowired @Qualifier("cubs")
-//    private Team away;
-//
-//    @Bean
-//    public Game game() {
-//        BaseballGame baseballGame = new BaseballGame(home, away);
-//        baseballGame.setDataSource(dataSource);
-//        return baseballGame;
-//    }
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private List<Team> teams;
+
+    @Bean
+    public NumberFormat nf() {
+        return NumberFormat.getCurrencyInstance();
+    }
+
+    @Bean
+    public Game game() {
+        BaseballGame baseballGame = new BaseballGame(redSox(), teams.get(0));
+        baseballGame.setDataSource(dataSource);
+        return baseballGame;
+    }
+
+    @Bean
+    public Team redSox() {
+        return new RedSox();
+    }
 }
